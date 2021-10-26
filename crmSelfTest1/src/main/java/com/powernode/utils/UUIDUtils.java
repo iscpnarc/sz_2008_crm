f845e26ad396fc5d2c30daec7f7a8cb7c0628ff1
muzi233 <muzi23323@163.com>
2021-10-26 21:21:02 +0800

    Utils工具类添加一乐一句输出语句

--- crmSelfTest1/src/main/java/com/powernode/utils/UUIDUtils.java
+++ crmSelfTest1/src/main/java/com/powernode/utils/UUIDUtils.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 public class UUIDUtils {
     // 返回32位随机字符串
     public static String getUUID() {
+        System.out.println("使用了UUID这个工具类");
         return UUID.randomUUID().toString().replace("-", "");
     }
 }
