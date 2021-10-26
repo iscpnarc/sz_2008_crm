d3551ca78547eb798d9bee945500a38ea0598638
muzi233 <muzi23323@163.com>
2021-10-26 21:25:49 +0800

    Utils工具类总新增加了测试git提交的方法

--- crmSelfTest1/src/main/java/com/powernode/utils/UUIDUtils.java
+++ crmSelfTest1/src/main/java/com/powernode/utils/UUIDUtils.java
@@ -8,4 +8,8 @@ public class UUIDUtils {
         System.out.println("使用了UUID这个工具类");
         return UUID.randomUUID().toString().replace("-", "");
     }
+
+    public static void test1(){
+        System.out.println("随意添加的第一个方法，为了测试IDEA中进行git的提交");
+    }
 }
